3613cefb921c735ff034c0b35b85cfb971d1bd74
danx <rushn24@gmail.com>
2024-05-21 21:59:59 -0400

    final with all working routes and models

--- routes/auth.js
+++ routes/auth.js
@@ -1,57 +1,49 @@
-const express = require('express');
-const router = new express.Router()
-const User = require('../models/user');
-const bcrypt = require('bcrypt')
-const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require('../config');
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
 
-/** POST /login - login: {username, password} => {token}
- *
- * Make sure to update their last-login!
- *
- **/
-router.post('/login', async (req, res, next) => {
-    try {
-        const { username, password } = req.body;
-        const result = await User.authenticate(username, password) 
-        return res.send(result)
-    } catch(e) {
-        return next(e)
-    }
-    
-})
+const User = require("../models/user");
+const {SECRET_KEY} = require("../config");
+const ExpressError = require("../expressError");
 
-/** POST /register - register user: registers, logs in, and returns token.
- *
- * {username, password, first_name, last_name, phone} => {token}.
- *
- *  Make sure to update their last-login!
- */
-router.post('/register', async (req, res, next) => {
-    try {
-        const { username, password, first_name, last_name, phone } = req.body;
-        const newUser = await User.register({ username, password, first_name, last_name, phone })
-        
-        if(newUser) {
-            const token = jwt.sign({
-                username, 
-                password: newUser.password, 
-                first_name,
-                last_name,
-                phone
-            }, SECRET_KEY)
-            newUser.token = token
-            return res.json(newUser)
-        }
-        
-    } catch(e) {
-        return next(e)
+
+/** login: {username, password} => {token} */
+
+router.post("/login", async function (req, res, next) {
+  try {
+    let {username, password} = req.body;
+    if (await User.authenticate(username, password)) {
+      let token = jwt.sign({username}, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json({token});
+    } else {
+      throw new ExpressError("Invalid username/password", 400);
     }
-    
+  }
+
+  catch (err) {
+    return next(err);
+  }
 });
 
-router.get('/test', (req, res) => {
-    res.send("this route is working")
-})
+/** register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ */
+
+router.post("/register", async function (req, res, next) {
+  try {
+    let {username} = await User.register(req.body);
+    let token = jwt.sign({username}, SECRET_KEY);
+    User.updateLoginTimestamp(username);
+    return res.json({token});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+
 
 module.exports = router;
